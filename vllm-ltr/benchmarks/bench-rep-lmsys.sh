8754861aa790fbfe4855120dd893ac1d8c230e5a
Ansh Sarkar <52290294+anshsarkar@users.noreply.github.com>
2026-03-06 22:42:14 -0500

    Final benchmark scripts

--- vllm-ltr/benchmarks/bench-rep-lmsys.sh
+++ vllm-ltr/benchmarks/bench-rep-lmsys.sh
@@ -46,7 +46,7 @@ sleep 60
 
 
 #opt-class82 (classification, bucket=100, 82 classes)
-CUDA_VISIBLE_DEVICES=0 python -m vllm.entrypoints.openai.api_server --model meta-llama/Meta-Llama-3-8B-Instruct --swap-space 100 --disable-log-requests --schedule-type tpt-class82-xxx --enable-chunked-prefill --enforce-eager --prefill-predictor-model-config MODEL/results/opt-125m-llama3-8b-lmsys-class-trainbucket100-b32/usage_config.json &
+CUDA_VISIBLE_DEVICES=0 python -m vllm.entrypoints.openai.api_server --model meta-llama/Meta-Llama-3-8B-Instruct --swap-space 100 --disable-log-requests --schedule-type tpt-class82-xxx --enable-chunked-prefill --enforce-eager --prefill-predictor-model-config MODEL/results/opt-125m-llama3-8b-lmsys-class-trainbucket100-b4/usage_config.json &
 sleep 120
 
 python benchmark_serving_real.py --backend vllm --model meta-llama/Meta-Llama-3-8B-Instruct  --tokenizer meta-llama/Meta-Llama-3-8B-Instruct --dataset lmsys-Meta-Llama-3-8B-Instruct-t1.0-s0-l8192-c10000-rFalse.jsonl --num-prompts -1 --request-time 60 --schedule-type tpt-class82-xxx --output-len -1 --request-rate 2 --result-dir RESULTS
